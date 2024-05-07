eecc0e54c1767f8612697d38d5bc0385f10303e0
lcfd <9001053+fedriz@users.noreply.github.com>
2024-05-07 20:27:39 +0200

    Fix and improve for v0.0.5

--- lua/trak/init.lua
+++ lua/trak/init.lua
@@ -1,6 +1,6 @@
-local util = require("trak.util")
+-- local util = require("trak.util")
+-- local popup = require("trak.ui.popup")
 local config = require("trak.config")
-local popup = require("trak.ui.popup")
 
 local trak = {}
 
@@ -9,8 +9,18 @@ trak.status = function()
   print(output)
 end
 
-trak.report = function()
-  local output = vim.fn.system("trak report projects")
+trak.report = function(flags)
+  return function()
+    local project_id = vim.fn.input("Project id: ")
+    if project_id then
+      local output = vim.fn.system(string.format("trak report project %s %s", project_id, flags))
+      print(output)
+    end
+  end
+end
+
+trak.report_all = function()
+  local output = vim.fn.system("trak report project all --today")
   print(output)
 end
 
@@ -21,9 +31,29 @@ local function bind_command()
     { desc = "Show the status of the current session.", nargs = "*" }
   )
   vim.api.nvim_create_user_command(
+    "TrakReportProject",
+    trak.report(),
+    { desc = "Show the report for a project.", nargs = "*" }
+  )
+  vim.api.nvim_create_user_command(
+    "TrakReportProjectMonth",
+    trak.report("--month"),
+    { desc = "Show the monthly report for a project.", nargs = "*" }
+  )
+  vim.api.nvim_create_user_command(
+    "TrakReportProjectWeek",
+    trak.report("--week"),
+    { desc = "Show the weekly report for a project.", nargs = "*" }
+  )
+  vim.api.nvim_create_user_command(
+    "TrakReportProjectToday",
+    trak.report("--today"),
+    { desc = "Show the today report for a project.", nargs = "*" }
+  )
+  vim.api.nvim_create_user_command(
     "TrakReportAll",
-    trak.report,
-    { desc = "Show the report for all projects.", nargs = "*" }
+    trak.report_all,
+    { desc = "Show the report for all projects you worked on today.", nargs = "*" }
   )
 end
 
